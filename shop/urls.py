b7a4bb23ab3d87c2ba7d04f91f4d1e0f8406d352
Andrewwxo <74658753+Andrewwxo@users.noreply.github.com>
2022-02-20 23:24:39 +0300

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
 ]
 
 if DEBUG:
-    urlpatterns.append(
-        path('__debug__/', include('debug_toolbar.urls'))
-    )
+#     urlpatterns.append(
+#         path('__debug__/', include('debug_toolbar.urls'))
+#     )
     urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
